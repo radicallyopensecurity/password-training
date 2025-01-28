186cdef0bfe3a0a6c0df0e4f1e1366420df5b41d
Bakeromso <>
2025-01-28 15:17:13 +0100

    improved windows start script

--- beef/start.bat
+++ beef/start.bat
@@ -1,10 +1,14 @@
 @echo off
+setlocal EnableDelayedExpansion
 
 REM Stop all containers of this demo
 call stop.bat
 
 REM Define the keywords to match in image names
-set KEYWORDS=beef|juice|caddy
+set KEYWORDS=beef juice caddy
+
+REM Initialize CONTAINER_IDS
+set CONTAINER_IDS=
 
 REM List all running containers and filter by image name
 for /f "tokens=1,2" %%i in ('docker ps --format "{{.ID}} {{.Image}}" ^| findstr /r "%KEYWORDS%"') do (
@@ -16,12 +20,12 @@ if defined CONTAINER_IDS (
     echo There are still containers running that are based on an image matching '%KEYWORDS%'. These are probably leftovers from an earlier version/run.
     echo more details by 'docker ps' filtered to these ids:
     echo.
-    for %%i in (%CONTAINER_IDS%) do (
+    for %%i in (!CONTAINER_IDS!) do (
         docker ps --filter "id=%%i" --format "table {{.ID}}\t{{.Image}}\t{{.Names}}\t{{.Status}}"
     )
     echo.
     echo To prevent problems, please stop and remove these containers
-    echo by running: 'docker rm -f %CONTAINER_IDS%'
+    echo by running: 'docker rm -f !CONTAINER_IDS!'
     exit /b -1
 )
 
